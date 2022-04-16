1d4546f9993c1098cc9340f6ba3cb9ce4be3431e
akilinjamam <akilinjamam@gmail.comm>
2022-04-16 21:44:29 +0600

    carosal-and-header-part-done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Component/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Component/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <br />
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Component/Banner/Banner.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import bannar1 from '../../bannar-img/the-innani-beach.jpg'
+import bannar2 from '../../bannar-img/the-sajek-valley.jpg'
+import bannar3 from '../../bannar-img/the-bandarban.jpg'
+import bannar4 from '../../bannar-img/the-tangoar-haor.jpg'
+import bannar5 from '../../bannar-img/the-rangamati.jpg'
+
+
+const Banner = () => {
+    return (
+        <div style={{ width: '90%', height: '200px', margin: 'auto', zIndex: '5' }}>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={bannar1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={bannar2}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={bannar3}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={bannar4}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={bannar5}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    );
+};
+
+export default Banner;
